5ba388506e01d7b1f0f153e9a359adf7df002596
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-08-03 00:10:38 -0300

    feat: created dataSanitization middleware for post methods
    feature details:
    
    (*) dataSanitization:
        - receives data from body and params.
        - strips html from data.
        - trim strings data.

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -11,10 +11,11 @@ import {
 } from "../schemas/auth.schema.js";
 
 import { schemaValidation } from "../middlewares/schemaValidation.js";
+import { dataSanitization } from "../middlewares/dataSanitization.js";
 
 const authRouter = Router();
 
-authRouter.post("/signup", schemaValidation(signUpSchema), signUp);
-authRouter.post("/signin", schemaValidation(signInSchema), signIn);
+authRouter.post("/signup", schemaValidation(signUpSchema), dataSanitization, signUp);
+authRouter.post("/signin", schemaValidation(signInSchema), dataSanitization, signIn);
 
 export default authRouter;
--- src/routes/url.routes.js
+++ src/routes/url.routes.js
@@ -9,12 +9,13 @@ import {
 
 import { urlSchema } from "../schemas/url.schema.js";
 import { schemaValidation } from "../middlewares/schemaValidation.js";
+import { dataSanitization } from "../middlewares/dataSanitization.js";
 
 const urlRouter = Router();
 
 urlRouter.get("/urls/:id", getUrlById);
 urlRouter.get("/urls/open/:shortUrl", openShortUrl);
-urlRouter.post("/urls/shorten", schemaValidation(urlSchema), createShortUrl);
+urlRouter.post("/urls/shorten", schemaValidation(urlSchema), dataSanitization, createShortUrl);
 urlRouter.delete("/urls/:id", deleteUrlById);
 
 export default urlRouter;
